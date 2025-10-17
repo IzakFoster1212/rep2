a894a4ab7c895ca35685687bdcb23d698fc1851c
IzakFoster <izakfoster@localhost.localdomain>
2025-10-17 16:01:12 +0700

    3

--- src/exercise3.c
+++ src/exercise3.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(int argc, char** argv) {
-    printf("Hello World!");
-    return 0;
+    int g = atoi(argv[1]);
+    float numb[10];
+    for(int i=0; i<10; i++) {
+        scanf("%f", &numb[i]);
+    }
+    g = g%10;
+    if (g<0) {
+        g+=10;
+    }
+    for(int i = 0; i<10; i++){
+        int index = (10 - g +i) %10;
+        printf("%.3lf\n", numb[index]);
+    }
 }
