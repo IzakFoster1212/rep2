2bf731d656298b1667666cfa635d687122e818c3
IzakFoster <izakfoster@localhost.localdomain>
2025-10-17 16:04:11 +0700

    4

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(int argc, char** argv) {
-    printf("Hello World!");
+    double x = atof(argv[1]);
+    int a = atoi(argv[2]);
+    double array[10];
+    for(int i = 0; i < 10; i++) {
+        scanf("%lf", &array[i]);
+    }
+    for(int i = 9; i > a; i--) {
+        array[i] = array[i-1];
+    }
+    array[a] = x;
+    for(int i = 0; i < 10; i++) {
+        printf("%lf ", array[i]);
+    }
+    printf("\n");
     return 0;
 }
