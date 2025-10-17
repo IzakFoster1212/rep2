508a38d679f351502d0307d5a4c36ade906103e8
IzakFoster <izakfoster@localhost.localdomain>
2025-10-17 15:55:21 +0700

    1

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main() {
+    double array[10];
+    double s = 0;
+    for(int i = 0; i < 10; i++) {
+        scanf("%lf", &array[i]);
+        s += array[i];
+    }
+    printf("%.3lf\n", s / 10);
     return 0;
 }
